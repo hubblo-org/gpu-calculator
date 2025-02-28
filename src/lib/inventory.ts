4fa3fcf974cea667b76033043ea6745b580875e4
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-02-28 16:54:46 +0100

    feat: number validation in form

--- src/lib/inventory.ts
+++ src/lib/inventory.ts
@@ -1,26 +1,45 @@
 import type { DataCenterInventoryElement } from "./types/pcr-cloud";
 import { InventoryCategories } from "./types/enums";
 
-export function addInventoryElement(inventory: DataCenterInventoryElement[]) {
-    const inventoryElementName = document.getElementById(
-      "inventory-element-name"
-    ) as HTMLInputElement;
-    const inventoryElementCategory = document.getElementById(
-      "inventory-element-category"
-    ) as HTMLInputElement;
-    const inventoryElementQuantity = document.getElementById(
-      "inventory-element-quantity"
-    ) as HTMLInputElement;
-    const inventoryElementLifespan = document.getElementById(
-      "inventory-element-lifespan"
-    ) as HTMLInputElement;
-    const inventoryElement: DataCenterInventoryElement = {
-      name: inventoryElementName?.value,
-      category: inventoryElementCategory?.value as InventoryCategories,
-      quantity: parseInt(inventoryElementQuantity?.value),
+export function validateNumber(element: HTMLInputElement) {
+  const maybeValidNumber = Number(element.value);
 
-      lifespan: parseInt(inventoryElementLifespan?.value)
-    };
-    inventory.push(inventoryElement);
-    return inventory;
+  if (isNaN(maybeValidNumber)) {
+    console.log("Not a Number");
+    return false;
+  } else {
+    return Number(element.value);
+  }
+}
+
+export function addInventoryElement(inventory: DataCenterInventoryElement[]) {
+  const inventoryElementName = document.getElementById(
+    "inventory-element-name"
+  ) as HTMLInputElement;
+  const inventoryElementCategory = document.getElementById(
+    "inventory-element-category"
+  ) as HTMLInputElement;
+  const inventoryElementQuantity = document.getElementById(
+    "inventory-element-quantity"
+  ) as HTMLInputElement;
+  const inventoryElementLifespan = document.getElementById(
+    "inventory-element-lifespan"
+  ) as HTMLInputElement;
+
+  const validatedQuantity = validateNumber(inventoryElementQuantity);
+  const validatedLifespan = validateNumber(inventoryElementLifespan);
+
+  if (!validatedQuantity || !validatedLifespan) {
+    return;
+  }
+
+  const inventoryElement: DataCenterInventoryElement = {
+    name: inventoryElementName.value,
+    category: inventoryElementCategory.value as InventoryCategories,
+    quantity: validatedQuantity,
+
+    lifespan: validatedLifespan
+  };
+  inventory.push(inventoryElement);
+  return inventory;
 }
