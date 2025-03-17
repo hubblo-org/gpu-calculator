2fede1f08ff5f99889a9471f831b625965563147
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-03-17 17:11:39 +0100

    fix: displaying full information for impact_criteria and scope value in label

--- src/lib/plots.ts
+++ src/lib/plots.ts
@@ -44,7 +44,7 @@ export function renderHorizontalBarPlot(
           x: xLabel,
           y: yLabel,
           fill: xLabel,
-          tip: true,
+          tip: { format: { y: (d) => `${d.replace("\n", " ")}` }, lineWidth: 100 },
           sort: { y: "x", order: "descending" }
         })
       ]
@@ -68,74 +68,3 @@ interface DataCenterImpactFactors {
   life_cycles: ImpactFactorsWithLC[];
   value: number;
 }
-/* export function renderTreemap(dc_data: DataCenterImpactFactors) {
-  let div = document.querySelector("#impactFactorsPlotTreemap");
-  div?.firstChild?.remove();
-  if (div) {
-    const width = 1154;
-    const height = 1154;
-    const color = d3.scaleOrdinal(
-      dc_data.life_cycles.map((impactFactor) => impactFactor.life_cycle_step),
-      d3.schemeTableau10
-    );
-
-    const root = d3.treemap().tile(d3.treemapSquarify).size([width, height]).padding(1).round(true)(
-      d3
-        .hierarchy(dc_data)
-        .sum((d) => d.value)
-        .sort((a, b) => b.data.value - a.data.value)
-    );
-    const svg = d3
-      .create("svg")
-      .attr("viewBow", [0, 0, width, height])
-      .attr("width", width)
-      .attr("height", height)
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
-
-    const leaf = svg
-      .selectAll("g")
-      .data(root.leaves())
-      .join("g")
-      .attr("transform", (d) => ` translate(${d.x0}, ${d.y0}`);
-
-    const format = d3.format(",d");
-    leaf.append("title").text(
-      (d) =>
-        `${d
-          .ancestors()
-          .reverse()
-          .map((d) => d.data.life_cycle_step)
-          .join(".")}\n${format(d.value)}`
-    );
-    /*
-    leaf
-      .append("rect")
-      .attr("id", (d) => (d.leafUid = DOM.uid("leaf")).id)
-      .attr("fill", (d) => {
-        while (d.depth > 1) d = d.parent;
-        return color(d.data.name);
-      })
-      .attr("fill-opacity", 0.6)
-      .attr("width", (d) => d.x1 - d.x0)
-      .attr("height", (d) => d.y1 - d.y0);
-
-    leaf
-      .append("clipPath")
-      .attr("id", (d) => (d.clipUid = DOM.uid("clip")).id)
-      .append("use")
-      .attr("xlink:href", (d) => d.leafUid.href);
-    leaf
-      .append("text")
-      .attr("clip-path", (d) => d.clipUid)
-      .selectAll("tspan")
-      .data((d) => d.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(format(d.value)))
-      .join("tspan")
-      .attr("x", 3)
-      .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
-      .attr("fill-opacity", (d, i, nodes) => (i === nodes.length - 1 ? 0.7 : null))
-      .text((d) => d);
-
-    return Object.assign(svg.node(), { scales: { color } }); 
-  }
-}
-*/
