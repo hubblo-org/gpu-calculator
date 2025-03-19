210295f8ae3ee98449f41a4fd44f4bd1cda5fd37
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-03-19 18:00:16 +0100

    feat: add demo route

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import FunctionalUnit from "$lib/components/FunctionalUnit.svelte";
+  import DataCenter from "$lib/components/DataCenter.svelte";
+</script>
+
+<FunctionalUnit />
+<DataCenter />
