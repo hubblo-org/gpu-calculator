cbd0905d389749661c638cc1b9ddb2ef3ee1957b
Benoit Petit <bpetit@hubblo.org>
2025-03-27 15:40:16 +0100

    fix: datacenter data through props

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import FunctionalUnit from "$lib/components/FunctionalUnit.svelte";
   import DataCenter from "$lib/components/DataCenter.svelte";
+  import { dataCenterCharacteristics } from "../../mocks/dc-data";
 </script>
 
 <FunctionalUnit />
-<DataCenter />
+<DataCenter dataCenter={dataCenterCharacteristics} />
