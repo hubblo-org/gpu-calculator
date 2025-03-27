46aec4306dbe47c194095ec6a74f439a886e293a
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-03-27 16:24:15 +0100

    refactor: use renderTreemap

--- src/lib/components/ResultsTreeMap.svelte
+++ src/lib/components/ResultsTreeMap.svelte
@@ -2,7 +2,7 @@
   import type { FunctionalUnitResultsRowWithLifeCycle, ImpactFactors } from "$lib/types/pcr-cloud";
   import type { IC } from "$lib/types/enums";
   import { ImpactCriterias, getAllImpactCriterias, getImpactCriteria } from "$lib/types/enums";
-  import * as d3 from "d3";
+  import { renderTreemap } from "$lib/treemap";
 
   interface Props {
     results: FunctionalUnitResultsRowWithLifeCycle[];
@@ -30,7 +30,7 @@
 
   const { results }: Props = $props();
 
-  const lifeCycleSteps = ["manufacturing", "transport", "use", "end-of-life"];
+  const lifeCycleSteps = ["Manufacturing", "Transport", "Use", "End-of-life"];
 
   let selectedImpactCriteria = $state(getImpactCriteria(ImpactCriterias.AcidificationPotential));
 
@@ -41,6 +41,7 @@
     return {
       name: "dc_data",
       children: lifeCycleSteps.map((lifeCycle) => {
+          
         const resultsByLifeCycle = results.filter((result) => result.life_cycle_step === lifeCycle);
         const resultsImpacts = resultsByLifeCycle.map((result) => {
           const leaf: Leaf = {
@@ -60,32 +61,9 @@
   const width = 1200;
   const height = 700;
 
-  const color = $derived(
-    d3.scaleOrdinal(
-      dcData.children.map((d) => d.name),
-      d3.schemeTableau10
-    )
-  );
-  const format = d3.format(",r");
-  const tile = d3.treemapSquarify;
-  const root = $derived.by(() => {
-    const r = d3
-      .treemap<DataCenterImpactFactors>()
-      .tile(tile)
-      .size([width, height])
-      .padding(1)
-      .round(true)(
-      d3
-        .hierarchy(dcData)
-        .sum((d) => d.value)
-        .sort((a, b) => b.data.value - a.data.value)
-    );
-    return r;
+  $effect(() => {
+    renderTreemap(dcData, width, height);
   });
-
-  function getFirstDepthParent(leaf: any): any {
-    return leaf.depth > 1 ? getFirstDepthParent(leaf.parent) : leaf;
-  }
 
   const productResults = results[0];
   const product = `${productResults.amount} ${productResults.unit}`;
@@ -97,47 +75,9 @@
 </p>
 
 <div id="treemap-legend">
-  {#each color.domain() as lifeCycleStep}
-    <p style="--color: {color(lifeCycleStep)}">{lifeCycleStep}</p>
-  {/each}
 </div>
 
 <div id="treemap">
-  <svg {width} {height} viewBox="0 0 {width} {height}">
-    {#each root.leaves() as leaf, leafIndex}
-      {@const nodes = leaf.data.name.split(/(?=[A-Z][a-z])|\s+/g).concat(format(leaf.data.value))}
-      <g transform="translate({leaf.x0}, {leaf.y0})">
-        <title>
-          {`${leaf
-            .ancestors()
-            .reverse()
-            .map((leaf) => leaf.data.name)
-            .join(".")}\n${format(leaf.data.value)}`}</title
-        >
-        <rect
-          id="rect-{leafIndex}"
-          fill={color(getFirstDepthParent(leaf).data.name)}
-          fill-opacity={0.8}
-          width={leaf.x1 - leaf.x0}
-          height={leaf.y1 - leaf.y0}
-        />
-        <clipPath id="clip-{leafIndex}">
-          <use href="#rect-{leafIndex}" />
-        </clipPath>
-        <text clip-path="url(#clip-{leafIndex})">
-          {#each nodes as node, nodeIndex}
-            <tspan
-              x="3"
-              y="{(nodes.length - 1) * 0.3 + 1.1 + nodeIndex * 0.9}em"
-              fill-opacity={nodeIndex === nodes.length - 1 ? 0.7 : null}
-            >
-              {node}
-            </tspan>
-          {/each}
-        </text>
-      </g>
-    {/each}
-  </svg>
 </div>
 
 <select
