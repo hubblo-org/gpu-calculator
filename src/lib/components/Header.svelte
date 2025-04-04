0a962a2c4944cf92b565c6193216ae8e0d3111a5
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-04-04 18:32:11 +0200

    feat: add contact and call-to-action

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,29 +1,22 @@
 <header>
   <div id="header-logo">
-    <a class="logo" href="https://hubblo.org/en" aria-label="Navigate to the Hubblo website">
+    <a class="logo" href="https://hubblo.org/en/" aria-label="Navigate to the Hubblo website">
       <img src="/media/logo.svg" alt="Hubblo logo" />
       <span>Hubblo</span>
     </a>
   </div>
+  <p>Want to know more about the environmental <br> evaluation of cloud services? Reach us at:</p>
+  <a href="https://hubblo.org/en/contact/" class="btn btn-primary btn-sm"> Contact </a>
 </header>
 
 <style>
-  header {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 0;
-    z-index: 1000;
-    width: 100%;
-    max-width: min(var(--wrapper-max-width), 100vw);
-    height: var(--header-height);
-    background-color: var(--color-background);
-    margin-inline: auto;
-    padding: var(--space-16) var(--gutter, 2rem);
-    display: flex;
-    column-gap: var(--gutter);
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: var(--border);
-    border-inline: var(--border);
+  p {
+    display: none;
+  }
+  @media (min-width: 800px) {
+    p {
+      margin-left: auto;
+      display: initial;
+    }
   }
 </style>
--- src/tests/header.test.ts
+++ src/tests/header.test.ts
@@ -2,6 +2,7 @@ import { cleanup, render, screen, within } from "@testing-library/svelte";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import Header from "$lib/components/Header.svelte";
 
+const callToActionText = "Want to know more about the environmental evaluation of cloud services?";
 describe("header component test suite", () => {
   beforeEach(() => render(Header));
   afterEach(() => cleanup());
@@ -14,5 +15,15 @@ describe("header component test suite", () => {
   it("should have a link to the Hubblo website", () => {
     const hubbloLink = screen.getByRole("link", { name: "Navigate to the Hubblo website" });
     expect(hubbloLink).toBeVisible();
+  });
+
+  it("should display a contact button", () => {
+    const contactButton = screen.getByRole("link", { name: "Contact" });
+    expect(contactButton).toBeVisible();
+  });
+
+  it("should display a call-to-action", () => {
+    const callToAction = screen.getByText(callToActionText, { exact: false });
+    expect(callToAction).toBeVisible();
   });
 });
