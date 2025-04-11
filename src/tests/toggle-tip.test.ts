3038e4782a52c5ab7932322f59979f9e178916af
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-04-11 14:39:52 +0200

    test: remove test as toggletip should stay visible after clicking on button

--- src/tests/toggle-tip.test.ts
+++ src/tests/toggle-tip.test.ts
@@ -6,7 +6,7 @@ import userEvent from "@testing-library/user-event";
 
 describe("toggle tip component test suite", () => {
   const description = dataCenterCharacteristics.totalSurface.description;
-  beforeEach(() => render(ToggleTip, { props: { info: description } }));
+  beforeEach(() => render(ToggleTip, { props: { info: description, source: "building-total-surface" } }));
   afterEach(() => cleanup());
   it("should display a description of the data center characteristic after the user interacted with a button next to the characteristic", async () => {
     const user = userEvent.setup();
@@ -17,17 +17,6 @@ describe("toggle tip component test suite", () => {
     await user.click(informationButton);
 
     expect(await screen.findByText(`${description}`)).toBeVisible();
-  });
-  it("should remove the description of the data center characteristic after the user clicked again on the button next to the characteristic", async () => {
-    const user = userEvent.setup();
-    const informationButton = screen.getByRole("button", {
-      name: "More information"
-    });
-
-    await user.click(informationButton);
-    await user.click(informationButton);
-
-    expect(screen.queryByText(`${description}`)).not.toBeInTheDocument();
   });
   it("should remove the description of the data center characteristic after the user clicked on the description", async () => {
     const user = userEvent.setup();
