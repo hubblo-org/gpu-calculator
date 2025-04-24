3c39faea55a06b6e0c155cf17b5da5ef9e3bcdc6
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-04-24 17:16:28 +0200

    test: remove log error for requestsubmit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { svelteTesting } from '@testing-library/svelte/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelteTesting } from "@testing-library/svelte/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit(), svelteTesting()],
@@ -9,6 +9,12 @@ export default defineConfig({
     include: ["src/**/*.{test,spec}.{js,ts}", "src/**/*.svelte.test.ts"],
     exclude: ["src/tests/e2e"],
     environment: "jsdom",
-    setupFiles: ["./vitest-setup-client.ts"]
+    setupFiles: ["./vitest-setup-client.ts"],
+    onConsoleLog(log: string) {
+      // Not implemented by jsdom, which is normal. Doing the usual mock does not seem to remove the error, not logging it instead.
+      if (log.includes("Error: Not implemented: HTMLFormElement.prototype.requestSubmit")) {
+        return false;
+      }
+    }
   }
 });
