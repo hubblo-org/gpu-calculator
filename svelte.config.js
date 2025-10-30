e59593a02628410bda63516e8a3785d3eeffb17d
Benjamin Dromard <benjamin.dromard@gmail.com>
2025-10-30 16:15:46 +0100

    test: create alias for mocks folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $mocks: "src/mocks"
+    }
+  }
 };
 
 export default config;
