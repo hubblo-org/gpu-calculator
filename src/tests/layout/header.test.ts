3e0a5c8945d802c04fdf0677541d23edb309aed2
B. Dromard <bdromard@tutanota.com>
2026-02-04 18:22:17 +0100

    chore: remove unused dependency

--- src/tests/layout/header.test.ts
+++ src/tests/layout/header.test.ts
@@ -1,4 +1,4 @@
-import { cleanup, render, screen, within } from "@testing-library/svelte";
+import { cleanup, render, screen } from "@testing-library/svelte";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import Header from "$lib/components/Header.svelte";
 
