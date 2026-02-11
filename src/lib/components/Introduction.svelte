67a44ce557f7d5bf3a1505624cb428a2396e25f9
B. Dromard <bdromard@tutanota.com>
2026-02-11 13:10:14 +0100

    docs: update english documentation

--- src/lib/components/Introduction.svelte
+++ src/lib/components/Introduction.svelte
@@ -1,7 +1,7 @@
 <section aria-labelledby="introduction-title">
 	<h2 id="introduction-title">Introduction</h2>
 	<p>
-This calculator estimates the embedded impacts (manufacturing, distribution, end of life) of a graphics card based on 16 PEF impact criteria (excluding the use phase). It is based on the parametric model developed for ADEME by Tide, Hubblo, and TND as part of research published in 2025 (Falk et al., 2025). The model is the result of extensive data collection, including the dismantling of a dozen graphics cards and the performance of a dozen elemental analyses.
+This calculator estimates the embedded impacts (manufacturing, distribution, end of life) of a graphics card based on 16 Product Environmental Footprint (PEF) impact criteria (excluding the use phase). It is based on the parametric model developed for ADEME by Tide, Hubblo, and TND as part of research published in 2025 (Falk et al., 2025). The model is the result of extensive data collection, including the dismantling of a dozen graphics cards and the performance of a dozen elemental analyses.
 	</p>
 
 	<div>
